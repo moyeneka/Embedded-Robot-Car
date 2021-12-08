3999fca025dcd504cb8c558c989f5967aefeb86a
Micheal Oyenekan <60825726+moyeneka@users.noreply.github.com>
2021-12-08 13:48:43 -0600

    coded parts

---
+++ embedded-systems-lab-main/embedded_systems_lab.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_sonar_example0.elf_on_local.tcl
@@ -0,0 +1,9 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+loadhw C:/Users/moyeneka/Desktop/embedded-systems-lab-main/embedded_systems_lab.sdk/base_soc_wrapper_hw_platform_0/system.hdf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+dow C:/Users/moyeneka/Desktop/embedded-systems-lab-main/embedded_systems_lab.sdk/Sonar_example0/Debug/Sonar_example0.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+con
---
+++ embedded-systems-lab-main/embedded_systems_lab.sdk/ArtyBot_example0/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_lightsensor_example0.elf_on_local.tcl
@@ -0,0 +1,9 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+loadhw C:/Users/moyeneka/Desktop/embedded-systems-lab-main/embedded_systems_lab.sdk/ArtyBot_example0/base_soc_wrapper_hw_platform_0/system.hdf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+dow C:/Users/moyeneka/Desktop/embedded_systems_lab.sdk/LightSensor_example0/Debug/LightSensor_example0.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Arty A7-35T 210319B3E860A"} -index 0
+con
